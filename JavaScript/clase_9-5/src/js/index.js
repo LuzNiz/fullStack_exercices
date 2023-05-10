b56f763519ba7125eaa711617b7812888f2f9e86
LuzNiz <luzniz37@gmail.com>
2023-05-09 23:05:10 -0300

    Subiendo ejercicio de la clase del 9/05. Front-End

---
+++ JavaScript/clase_9-5/src/js/index.js
@@ -0,0 +1,28 @@
+import data from "./data.js";
+console.log(data);
+
+let dataJson = JSON.parse(data);
+let contenedor = document.getElementById("cards-container");
+console.log(dataJson);
+
+function createElement(name, picture, address){
+    const card = document.createElement('div');
+    card.classList.add("card");
+    card.innerHTML =`
+    <h2>${name}</h2>
+    <img src= ${picture} alt= ${name}></img>
+    <h3>${address}</h3>
+    `;
+    return card;
+}
+
+dataJson.map(function(obj){
+    let name = `${obj.name.first} ${obj.name.last}`;
+    let img = obj.picture.medium;
+    let location = `${obj.location.street.name} ${obj.location.street.number}`
+    let newCard = createElement(name, img, location);
+    contenedor.appendChild(newCard);
+})
+
+
+
